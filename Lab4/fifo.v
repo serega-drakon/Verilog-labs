d3f07a48882822fa3c81115ab266f4e4355f9c76
serega-drakon <megaserega228@gmail.com>
2023-07-05 05:29:31 +0300

    оа оа

--- Lab4/fifo.v
+++ Lab4/fifo.v
@@ -16,74 +16,74 @@ module fifo #(
 
     assign wr_ready = !(len == FIFO_DEPTH);
 
-    //вынес за знаки условий общие действия
     always @(posedge clk) begin
-        if(wr_en) begin
-            array[0] <= wr_data;
-        end
-        if(rd_en) begin
+        if(rd_en)
             rd_data <= array[0];
-            rd_val <= !(len == 0);
-        end
     end
 
-    //далее пошли различия по алгоритму действий
-    //RESET
-    always @(posedge clk) begin
-        if(reset) begin
+    generate if(FIFO_DEPTH > 1) begin : ifgenStack
+        always @(posedge clk) begin
             if(wr_en) begin
-                len <= 1;
+                array[0] <= wr_data;
             end
-            else begin
+            else if(reset) begin
                 array[0] <= 0;
-                len <= 0;
+            end
+            else if(rd_en) begin
+                array[0] <= array[1];
             end
         end
     end
+    else if(FIFO_DEPTH == 1) begin : ifgenSimpleStack
+        always  @(posedge clk) begin
+            if(wr_en) begin
+                array[0] <= wr_data;
+            end
+            else if(reset) begin
+                array[0] <= 0;
+            end
+        end
+    end
+    endgenerate
 
     genvar i;
-    generate for(i = 1; i < FIFO_DEPTH; i = i + 1)
-        always @(posedge clk)
-            if(reset)
-                array[i] <= 0;
+    generate for(i = 1; i < FIFO_DEPTH; i = i + 1) begin : loopArray
+        if(i != FIFO_DEPTH - 1) begin
+            always @(posedge clk)
+                if(reset)
+                    array[i] <= 0;
+                else if(wr_en && !rd_en)
+                    array[i] <= array[i - 1];
+                else if(!wr_en && rd_en)
+                    array[i] <= array[i + 1];
+        end
+        else begin
+            always @(posedge clk)
+                if(reset)
+                    array[i] <= 0;
+                else if(wr_en && !rd_en)
+                    array[i] <= array[i - 1];
+        end
+    end
     endgenerate
-
-    //READ
-    wire onlyRD;
-    assign onlyRD = !reset && !wr_en && rd_en;
 
     always @(posedge clk) begin
-        if(onlyRD && len > 0)
-                len <= len - 1;
+        if(rd_en)
+            rd_val <= !(len == 0);
     end
 
-    generate for(i = 0; i < FIFO_DEPTH - 1; i = i + 1)
-        always @(posedge clk)
-            if(onlyRD)
-                array[i] <= array[i + 1];
-    endgenerate
-
-    //WRITE
-    wire onlyWR;
-    assign onlyWR = !reset && !rd_en && wr_en;
-
-    always @(posedge clk) begin
-        if(onlyWR && len < FIFO_DEPTH)
-                len <= len + 1;
-    end
-
-    generate for(i = 0; i < FIFO_DEPTH - 1; i = i + 1)
-        always @(posedge clk)
-            if(onlyWR)
-                array[i + 1] <= array[i];
-    endgenerate
-
-    //READ&WRITE
-    wire onlyRDWR;
-    assign onlyRDWR = !reset && rd_en && wr_en;
-
-    always @(posedge clk)
-        if(onlyRDWR && len == 0)
+    always  @(posedge clk) begin
+        if(reset) begin
+            if(wr_en)
+                len <= 1;
+            else
+                len <= 0;
+        end
+        else if(!wr_en && rd_en && len > 0)
+            len <= len - 1;
+        else if(wr_en && !rd_en && len < FIFO_DEPTH)
+            len <= len + 1;
+        else if(wr_en && rd_en && len == 0)
             len <= 1;
+    end
 endmodule
-//наверное я фигово оформил, пока чето красивое пытаюсь выдавать
