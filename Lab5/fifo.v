d0e289d8600b60977949177b11481a97bfe2705a
serega-drakon <megaserega228@gmail.com>
2023-07-06 09:48:32 +0300

    оа оа

--- Lab5/fifo.v
+++ Lab5/fifo.v
@@ -12,27 +12,49 @@ module fifo #(
     output reg rd_val
 );
     reg [DATA_WIDTH - 1 : 0] array [FIFO_DEPTH - 1 : 0];
-    reg [DATA_WIDTH - 1 : 0] flag_val;
+    reg flag_val [FIFO_DEPTH - 1 : 0];
 
-    genvar i;
-    generate for(i = 0; i < FIFO_DEPTH; i = i + 1) begin : loopArray
-        if(i == 0) begin
-            always @(posedge clk) begin
+    assign wr_ready = rd_en | ~flag_val[FIFO_DEPTH - 1];
 
-            end
+    always @(posedge clk) begin
+        if(wr_en) begin
+            array[0] <= wr_data;
+            flag_val[0] <= 1;
         end
-        else if(i == FIFO_DEPTH - 1) begin
-            always @(posedge clk) begin
-
-            end
+        else if(reset) begin
+            array[0] <= 0;
+            flag_val[0] <= 0;
         end
         else begin
-            always @(posedge clk) begin
+            if(rd_en) begin
+                flag_val[0] <= 0;
+            end
+        end
+    end
 
+    genvar i;
+    generate for(i = 1; i < FIFO_DEPTH; i = i + 1) begin : loopArray
+        always @(posedge clk) begin
+            if(reset) begin
+                array[i] <= 0;
+                flag_val[i] <= 0;
+            end
+            else if(wr_en || rd_en) begin
+                array[i] <= array[i - 1];
+                flag_val[i] <= flag_val[i - 1];
             end
         end
     end
     endgenerate
 
-
+    always @(posedge clk) begin
+        if(reset) begin
+            rd_data <= 0;
+            rd_val <= 0;
+        end
+        else if(rd_en) begin
+            rd_data <= array[FIFO_DEPTH - 1];
+            rd_val <= flag_val[FIFO_DEPTH - 1];
+        end
+    end
     endmodule
